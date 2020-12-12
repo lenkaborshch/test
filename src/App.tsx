2080a8e23bad644b5f7252f083023c26c7c5f807
Helen <melnikelena6969@gmail.com>
2020-12-12 22:39:22 +0200

    lorem done

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export const App = () => {
           commit 2
           commit 2
           commit 2
-        Hey hey
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto doloremque dolorum ex facilis fugiat fugit harum, hic ipsa maiores molestiae molestias officia omnis quidem quos tempore veniam vero.
       </header>
     </div>
   );
