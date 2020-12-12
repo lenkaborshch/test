88ab9677de69cfb5cf5d5eaf33789283574bf70a
Helen <melnikelena6969@gmail.com>
2020-12-12 11:39:12 +0200

    Create const

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
