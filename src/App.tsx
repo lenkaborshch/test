412b22ab6208ec2fd02371a428ddf8d61b52c600
Helen Borshch <melnikelena6969@gmail.com>
2021-09-18 11:40:08 +0300

    new comm

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 export const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        commit 1
-          commit 2
-          commit 2
+    return (
+        <div className="App">
+            <header className="App-header">
+                commit 1
+                commit 2
+                commit 2
 
-        moya ofigennaya ficha gotova
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto doloremque dolorum ex facilis fugiat fugit harum, hic ipsa maiores molestiae molestias officia omnis quidem quos tempore veniam vero.
-        moya ofigemcndssjcjsd
+                moya ofigennaya ficha gotova
+                cvmdsjvndjsnk moya ofigemcndssjcjsd
 
-        commit something else - borshch
-      </header>
-    </div>
-  );
+                commit something else - borshch
+            </header>
+        </div>
+    )
 }
 
