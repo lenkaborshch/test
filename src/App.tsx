26b470342374cba5a9c813eeb01e4dd2b628b59b
Helen Borshch <melnikelena6969@gmail.com>
2021-09-18 11:20:44 +0300

    New commit from lena

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 export const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        commit 1
-          commit 2
-          commit 2
-          commit 2
-          commit 2
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto doloremque dolorum ex facilis fugiat fugit harum, hic ipsa maiores molestiae molestias officia omnis quidem quos tempore veniam vero.
-
-        moya ofigennaya ficha gotova
-
-
-        commit something else - borshch
-      </header>
-    </div>
-  );
+    const date = new Date()
+    console.log(date)
     return (
         <div className="App">
             <header className="App-header">
+                commit 1
+                commit 2
+                commit 2
+                commit 2
+                commit 2
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto doloremque dolorum ex
+                facilis fugiat fugit harum, hic ipsa maiores molestiae molestias officia omnis quidem quos tempore
+                veniam vero.
 
-                bubkaaaaaaaaaaaaaaaaaaaaaa
+                moya ofigennaya ficha gotova
+
+
+                commit something else - borshch
             </header>
         </div>
     )
