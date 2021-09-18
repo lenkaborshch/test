f93e34151754a8a47c75ee47c19671a675466759
Helen Borshch <melnikelena6969@gmail.com>
2021-09-18 11:27:46 +0300

    Туц

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,8 @@ export const App = () => {
         commit 1
           commit 2
           commit 2
-          commit 2
-          commit 2
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto doloremque dolorum ex facilis fugiat fugit harum, hic ipsa maiores molestiae molestias officia omnis quidem quos tempore veniam vero.
 
         moya ofigennaya ficha gotova
-
 
         commit something else - borshch
       </header>
